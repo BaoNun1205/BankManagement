33463c89455722373fe7519b183b42a100be0f70
BaoNun1205 <baole.120503@gmail.com>
2023-03-14 12:07:52 +0700

    xong phần gửi tiết kiệm

--- DOAN_Nhom4/FrmTietkiem.cs
+++ DOAN_Nhom4/FrmTietkiem.cs
@@ -222,7 +222,7 @@ namespace DOAN_Nhom4
             tknh.SoDu = tknh.SoDu - tienGoc;
             tknhDAO.Sua(tknh);
             MessageBox.Show("Tạo tài khoản tiết kiện thành công.", "Thông báo", MessageBoxButtons.OK);
-            FrmTietkiem frmtietkiem = new FrmTietkiem(ngdung, tknh);
+            FrmTietkiem frmtietkiem = new FrmTietkiem(kh, tknh);
             Hide();
             frmtietkiem.ShowDialog();
             Close();
