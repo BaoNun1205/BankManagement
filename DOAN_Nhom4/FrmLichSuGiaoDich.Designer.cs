b125c7c8aceeb8b6f508c1ed39a0562e59b734c3
hihieu04 <hihieu04@gmail.com>
2023-03-14 13:47:25 +0700

    frm lsgd

---
+++ DOAN_Nhom4/FrmLichSuGiaoDich.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace DOAN_Nhom4
+{
+    partial class FrmLichSuGiaoDich
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pn_TienVao = new System.Windows.Forms.Panel();
+            this.pn_ToanBo = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btn_TienRa = new CustomButton.VBButton();
+            this.btn_TienVao = new CustomButton.VBButton();
+            this.btn_ToanBo = new CustomButton.VBButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.vbButton3 = new CustomButton.VBButton();
+            this.vbButton2 = new CustomButton.VBButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.vbButton1 = new CustomButton.VBButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.pn_ToanBo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.pn_TienVao);
+            this.panel1.Controls.Add(this.pn_ToanBo);
+            this.panel1.Controls.Add(this.btn_TienRa);
+            this.panel1.Controls.Add(this.btn_TienVao);
+            this.panel1.Controls.Add(this.btn_ToanBo);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.vbButton3);
+            this.panel1.Controls.Add(this.vbButton2);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.vbButton1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-9, -23);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(783, 809);
+            this.panel1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(52, 423);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(701, 366);
+            this.dataGridView1.TabIndex = 39;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel2.Location = new System.Drawing.Point(517, 416);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(236, 1);
+            this.panel2.TabIndex = 38;
+            // 
+            // pn_TienVao
+            // 
+            this.pn_TienVao.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pn_TienVao.Location = new System.Drawing.Point(286, 416);
+            this.pn_TienVao.Name = "pn_TienVao";
+            this.pn_TienVao.Size = new System.Drawing.Size(236, 1);
+            this.pn_TienVao.TabIndex = 37;
+            // 
+            // pn_ToanBo
+            // 
+            this.pn_ToanBo.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pn_ToanBo.Controls.Add(this.panel3);
+            this.pn_ToanBo.Location = new System.Drawing.Point(52, 416);
+            this.pn_ToanBo.Name = "pn_ToanBo";
+            this.pn_ToanBo.Size = new System.Drawing.Size(236, 1);
+            this.pn_ToanBo.TabIndex = 36;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(236, 1);
+            this.panel3.TabIndex = 24;
+            // 
+            // btn_TienRa
+            // 
+            this.btn_TienRa.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btn_TienRa.BackgroundColor = System.Drawing.Color.LightSlateGray;
+            this.btn_TienRa.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btn_TienRa.BorderRadius = 0;
+            this.btn_TienRa.BorderSize = 0;
+            this.btn_TienRa.FlatAppearance.BorderSize = 0;
+            this.btn_TienRa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_TienRa.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_TienRa.ForeColor = System.Drawing.Color.White;
+            this.btn_TienRa.Location = new System.Drawing.Point(521, 375);
+            this.btn_TienRa.Name = "btn_TienRa";
+            this.btn_TienRa.Size = new System.Drawing.Size(232, 35);
+            this.btn_TienRa.TabIndex = 35;
+            this.btn_TienRa.Text = "Tiền ra";
+            this.btn_TienRa.TextColor = System.Drawing.Color.White;
+            this.btn_TienRa.UseVisualStyleBackColor = false;
+            // 
+            // btn_TienVao
+            // 
+            this.btn_TienVao.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btn_TienVao.BackgroundColor = System.Drawing.Color.LightSlateGray;
+            this.btn_TienVao.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btn_TienVao.BorderRadius = 0;
+            this.btn_TienVao.BorderSize = 0;
+            this.btn_TienVao.FlatAppearance.BorderSize = 0;
+            this.btn_TienVao.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_TienVao.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_TienVao.ForeColor = System.Drawing.Color.White;
+            this.btn_TienVao.Location = new System.Drawing.Point(286, 375);
+            this.btn_TienVao.Name = "btn_TienVao";
+            this.btn_TienVao.Size = new System.Drawing.Size(236, 35);
+            this.btn_TienVao.TabIndex = 34;
+            this.btn_TienVao.Text = "Tiền vào";
+            this.btn_TienVao.TextColor = System.Drawing.Color.White;
+            this.btn_TienVao.UseVisualStyleBackColor = false;
+            // 
+            // btn_ToanBo
+            // 
+            this.btn_ToanBo.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btn_ToanBo.BackgroundColor = System.Drawing.Color.LightSlateGray;
+            this.btn_ToanBo.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btn_ToanBo.BorderRadius = 0;
+            this.btn_ToanBo.BorderSize = 0;
+            this.btn_ToanBo.FlatAppearance.BorderSize = 0;
+            this.btn_ToanBo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_ToanBo.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_ToanBo.ForeColor = System.Drawing.Color.White;
+            this.btn_ToanBo.Location = new System.Drawing.Point(52, 375);
+            this.btn_ToanBo.Name = "btn_ToanBo";
+            this.btn_ToanBo.Size = new System.Drawing.Size(236, 35);
+            this.btn_ToanBo.TabIndex = 33;
+            this.btn_ToanBo.Text = "Toàn bộ";
+            this.btn_ToanBo.TextColor = System.Drawing.Color.White;
+            this.btn_ToanBo.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DOAN_Nhom4.Properties.Resources.z4156388208487_6cfd569e12217b8073a7fbfcc22c322f1;
+            this.pictureBox1.Location = new System.Drawing.Point(342, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(136, 125);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 27;
+            this.pictureBox1.TabStop = false;
+            // 
+            // vbButton3
+            // 
+            this.vbButton3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.vbButton3.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.vbButton3.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.vbButton3.BorderRadius = 20;
+            this.vbButton3.BorderSize = 0;
+            this.vbButton3.FlatAppearance.BorderSize = 0;
+            this.vbButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.vbButton3.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.vbButton3.ForeColor = System.Drawing.Color.White;
+            this.vbButton3.Location = new System.Drawing.Point(418, 306);
+            this.vbButton3.Name = "vbButton3";
+            this.vbButton3.Size = new System.Drawing.Size(335, 50);
+            this.vbButton3.TabIndex = 32;
+            this.vbButton3.Text = "Tìm kiếm ";
+            this.vbButton3.TextColor = System.Drawing.Color.White;
+            this.vbButton3.UseVisualStyleBackColor = false;
+            // 
+            // vbButton2
+            // 
+            this.vbButton2.BackColor = System.Drawing.Color.LightSlateGray;
+            this.vbButton2.BackgroundColor = System.Drawing.Color.LightSlateGray;
+            this.vbButton2.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.vbButton2.BorderRadius = 20;
+            this.vbButton2.BorderSize = 0;
+            this.vbButton2.FlatAppearance.BorderSize = 0;
+            this.vbButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.vbButton2.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.vbButton2.ForeColor = System.Drawing.Color.White;
+            this.vbButton2.Location = new System.Drawing.Point(52, 306);
+            this.vbButton2.Name = "vbButton2";
+            this.vbButton2.Size = new System.Drawing.Size(360, 50);
+            this.vbButton2.TabIndex = 31;
+            this.vbButton2.Text = "Xuất excel";
+            this.vbButton2.TextColor = System.Drawing.Color.White;
+            this.vbButton2.UseVisualStyleBackColor = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox1.Font = new System.Drawing.Font("Sitka Banner", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1 ngày gần đây nhất",
+            "3 ngày gần đây nhất",
+            "7 ngày gần đây nhất",
+            "30 ngày gần đây nhất",
+            "100 ngày gần đây nhất"});
+            this.comboBox1.Location = new System.Drawing.Point(244, 216);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(465, 41);
+            this.comboBox1.TabIndex = 30;
+            // 
+            // vbButton1
+            // 
+            this.vbButton1.BackColor = System.Drawing.Color.CadetBlue;
+            this.vbButton1.BackgroundColor = System.Drawing.Color.CadetBlue;
+            this.vbButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.vbButton1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.vbButton1.BorderRadius = 20;
+            this.vbButton1.BorderSize = 0;
+            this.vbButton1.FlatAppearance.BorderSize = 0;
+            this.vbButton1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.vbButton1.Font = new System.Drawing.Font("Sitka Banner", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.vbButton1.ForeColor = System.Drawing.Color.Black;
+            this.vbButton1.Location = new System.Drawing.Point(52, 183);
+            this.vbButton1.Name = "vbButton1";
+            this.vbButton1.Size = new System.Drawing.Size(701, 106);
+            this.vbButton1.TabIndex = 29;
+            this.vbButton1.Text = "     Thời gian";
+            this.vbButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.vbButton1.TextColor = System.Drawing.Color.Black;
+            this.vbButton1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(52, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 21);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Lịch sử giao dịch tài khoản";
+            // 
+            // FrmLichSuGiaoDich
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 776);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmLichSuGiaoDich";
+            this.Text = "FrmLichSuGiaoDich";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.pn_ToanBo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private DataGridView dataGridView1;
+        private Panel panel2;
+        private Panel pn_TienVao;
+        private Panel pn_ToanBo;
+        private Panel panel3;
+        private CustomButton.VBButton btn_TienRa;
+        private CustomButton.VBButton btn_TienVao;
+        private CustomButton.VBButton btn_ToanBo;
+        private PictureBox pictureBox1;
+        private CustomButton.VBButton vbButton3;
+        private CustomButton.VBButton vbButton2;
+        private ComboBox comboBox1;
+        private CustomButton.VBButton vbButton1;
+        private Label label1;
+    }
+}
