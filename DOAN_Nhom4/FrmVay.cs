d6514c6933a7d2c85e143f437a63adde6c742f8c
hihieu04 <hihieu04@gmail.com>
2023-03-23 23:00:19 +0700

    vay tiep

--- DOAN_Nhom4/FrmVay.cs
+++ DOAN_Nhom4/FrmVay.cs
@@ -16,5 +16,15 @@ namespace DOAN_Nhom4
         {
             InitializeComponent();
         }
+
+        private void FrmVay_Load(object sender, EventArgs e)
+        {
+            string[] danhxung = {"Ông", "Bà"};
+            string[] nghe = { "Chọn"};
+            string[] thunhap = {"Chọn" ,"<= 3 triệu đồng", ">= 3-5 triệu đồng", ">= 5-10 triệu đồng", ">= 10-20 triệu đồng", ">= 20 triệu đồng" };
+            cb_DanhXung.DataSource = danhxung;
+            cb_NgheNghiep.DataSource = nghe;
+            cb_ThuNhap.DataSource = thunhap;
+        }
     }
 }
