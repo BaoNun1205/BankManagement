344b8e25844bc5c6c2c1c716ed46ba85622b5b00
hieulag15 <hieuthanhtran12@gmail.com>
2023-03-27 13:06:23 +0700

    the hhb visa

--- DOAN_Nhom4/FrmThongTinTheHHBVisaFlexi.Designer.cs
+++ DOAN_Nhom4/FrmThongTinTheHHBVisaFlexi.Designer.cs
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ptbQuayLai = new System.Windows.Forms.PictureBox();
+            this.elipseControl1 = new ElipseToolDemo.ElipseControl();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbQuayLai)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ptbQuayLai
+            // 
+            this.ptbQuayLai.Image = global::DOAN_Nhom4.Properties.Resources.left_arrow1;
+            this.ptbQuayLai.Location = new System.Drawing.Point(1, 1);
+            this.ptbQuayLai.Name = "ptbQuayLai";
+            this.ptbQuayLai.Size = new System.Drawing.Size(41, 45);
+            this.ptbQuayLai.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbQuayLai.TabIndex = 66;
+            this.ptbQuayLai.TabStop = false;
+            // 
+            // elipseControl1
+            // 
+            this.elipseControl1.CornerRadius = 30;
+            this.elipseControl1.TargetControl = null;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(69)))), ((int)(((byte)(158)))));
+            this.label2.Location = new System.Drawing.Point(432, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(255, 39);
+            this.label2.TabIndex = 67;
+            this.label2.Text = "HHB VISA FLEXI";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(69)))), ((int)(((byte)(158)))));
+            this.panel1.Location = new System.Drawing.Point(1, 46);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1122, 5);
+            this.panel1.TabIndex = 68;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(69)))), ((int)(((byte)(158)))));
+            this.panel2.Location = new System.Drawing.Point(480, 51);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(5, 545);
+            this.panel2.TabIndex = 69;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DOAN_Nhom4.Properties.Resources.hhb_visa_Flexi;
+            this.pictureBox1.Location = new System.Drawing.Point(37, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(397, 217);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 70;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FrmThongTinTheHHBVisaFlexi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1123, 681);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ptbQuayLai);
+            this.Name = "FrmThongTinTheHHBVisaFlexi";
             this.Text = "FrmThongTinTheHHBVisaFlexi";
+            ((System.ComponentModel.ISupportInitialize)(this.ptbQuayLai)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private PictureBox ptbQuayLai;
+        private ElipseToolDemo.ElipseControl elipseControl1;
+        private Label label2;
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
     }
 }
