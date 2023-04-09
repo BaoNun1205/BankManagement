d240282336ca84e1911593baed286b5fee3d367a
hihieu04 <hihieu04@gmail.com>
2023-04-09 23:38:38 +0700

    vasy

--- DOAN_Nhom4/FrmVay.cs
+++ DOAN_Nhom4/FrmVay.cs
@@ -89,7 +89,8 @@ namespace DOAN_Nhom4
                     {
                         if (int.Parse(txt_ThoiGianVay.Text) >= 6 && int.Parse(txt_ThoiGianVay.Text) <= 60)
                         {
-                            if (ttNgDungDAO.TKValid(nguoiDung.SoTK) == null)
+                            ThongTinNguoiDungVay tmp = ttNgDungDAO.TKValid(nguoiDung.SoTK);
+                            if ( tmp == null || tmp.ThoiGianVay == 0)
                             {
                                 ThongTinNguoiDungVay ttNgDung = new ThongTinNguoiDungVay(nguoiDung.SoTK, cb_DanhXung.Text, nguoiDung.TenTK, nguoiDung.Cccd, txt_DiaChi.Text, nguoiDung.Sdt, nguoiDung.Email, cb_NgheNghiep.Text, cb_ThuNhap.Text, cb_SPVay.Text, BigInteger.Parse(txt_SoTienVay.Text), int.Parse(txt_ThoiGianVay.Text), s, txt_NgayVay.Value);
                                 FrmXacNhanThongTinNguoiDungVay xacnhan = new FrmXacNhanThongTinNguoiDungVay(nguoiDung, tknh, ttNgDung, pnlNguoiDung);
