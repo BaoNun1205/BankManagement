4e3a460205094f5a42dae89df031a70d23f3d13b
BaoNun1205 <baole.120503@gmail.com>
2023-04-12 01:13:44 +0700

    trang chủ

--- DOAN_Nhom4/FrmTrangchu.Designer.cs
+++ DOAN_Nhom4/FrmTrangchu.Designer.cs
@@ -33,26 +33,28 @@
             this.label2 = new System.Windows.Forms.Label();
             this.ptbLoGo = new System.Windows.Forms.PictureBox();
             this.lblXemLsgd = new System.Windows.Forms.Label();
-            this.lblTime = new System.Windows.Forms.Label();
             this.lblDate = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblTime = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.ptbLoGo)).BeginInit();
             this.SuspendLayout();
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(364, 624);
+            this.label2.Location = new System.Drawing.Point(676, 1331);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.Size = new System.Drawing.Size(0, 32);
             this.label2.TabIndex = 13;
             // 
             // ptbLoGo
             // 
             this.ptbLoGo.Image = global::DOAN_Nhom4.Properties.Resources.z4156388208487_6cfd569e12217b8073a7fbfcc22c322f1;
-            this.ptbLoGo.Location = new System.Drawing.Point(472, 215);
+            this.ptbLoGo.Location = new System.Drawing.Point(877, 459);
+            this.ptbLoGo.Margin = new System.Windows.Forms.Padding(6);
             this.ptbLoGo.Name = "ptbLoGo";
-            this.ptbLoGo.Size = new System.Drawing.Size(195, 154);
+            this.ptbLoGo.Size = new System.Drawing.Size(362, 329);
             this.ptbLoGo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.ptbLoGo.TabIndex = 14;
             this.ptbLoGo.TabStop = false;
@@ -62,32 +64,23 @@
             this.lblXemLsgd.AutoSize = true;
             this.lblXemLsgd.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.lblXemLsgd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(69)))), ((int)(((byte)(158)))));
-            this.lblXemLsgd.Location = new System.Drawing.Point(917, 9);
+            this.lblXemLsgd.Location = new System.Drawing.Point(1703, 19);
+            this.lblXemLsgd.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblXemLsgd.Name = "lblXemLsgd";
-            this.lblXemLsgd.Size = new System.Drawing.Size(210, 30);
+            this.lblXemLsgd.Size = new System.Drawing.Size(421, 57);
             this.lblXemLsgd.TabIndex = 15;
             this.lblXemLsgd.Text = "Xem lịch sử giao dịch";
             this.lblXemLsgd.Click += new System.EventHandler(this.lblXemLsgd_Click);
-            // 
-            // lblTime
-            // 
-            this.lblTime.AutoSize = true;
-            this.lblTime.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblTime.ForeColor = System.Drawing.Color.Black;
-            this.lblTime.Location = new System.Drawing.Point(487, 384);
-            this.lblTime.Name = "lblTime";
-            this.lblTime.Size = new System.Drawing.Size(105, 37);
-            this.lblTime.TabIndex = 16;
-            this.lblTime.Text = "lblTime";
             // 
             // lblDate
             // 
             this.lblDate.AutoSize = true;
             this.lblDate.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.lblDate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(69)))), ((int)(((byte)(158)))));
-            this.lblDate.Location = new System.Drawing.Point(428, 420);
+            this.lblDate.Location = new System.Drawing.Point(795, 896);
+            this.lblDate.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblDate.Name = "lblDate";
-            this.lblDate.Size = new System.Drawing.Size(103, 37);
+            this.lblDate.Size = new System.Drawing.Size(199, 72);
             this.lblDate.TabIndex = 17;
             this.lblDate.Text = "lblDate";
             // 
@@ -95,21 +88,33 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTime.ForeColor = System.Drawing.Color.Black;
+            this.lblTime.Location = new System.Drawing.Point(965, 824);
+            this.lblTime.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(172, 72);
+            this.lblTime.TabIndex = 18;
+            this.lblTime.Text = "label1";
+            // 
             // FrmTrangchu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1139, 720);
-            this.Controls.Add(this.lblDate);
+            this.ClientSize = new System.Drawing.Size(2115, 1536);
             this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblDate);
             this.Controls.Add(this.lblXemLsgd);
             this.Controls.Add(this.ptbLoGo);
             this.Controls.Add(this.label2);
             this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+            this.Margin = new System.Windows.Forms.Padding(6, 2, 6, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FrmTrangchu";
@@ -127,8 +132,8 @@
         private Label label2;
         private PictureBox ptbLoGo;
         private Label lblXemLsgd;
-        private Label lblTime;
         private Label lblDate;
         private System.Windows.Forms.Timer timer1;
+        private Label lblTime;
     }
 }
--- DOAN_Nhom4/FrmTrangchu.cs
+++ DOAN_Nhom4/FrmTrangchu.cs
@@ -28,11 +28,20 @@
         }
 
         private void FrmTrangchu_Load(object sender, EventArgs e)
-        {
-
+        {          
             timer1.Start();
+
             lblTime.Text = DateTime.Now.ToLongTimeString();
             lblDate.Text = DateTime.Now.ToLongDateString();
+
+            int x = (this.Size.Width - lblTime.Width) / 2;
+            int y = (this.Size.Height - lblTime.Height) / 2;
+
+            int x2 = (this.Size.Width - lblDate.Width) / 2;
+            int y2 = (this.Size.Height - lblDate.Height) / 2;
+
+            lblTime.Location = new Point(x, y + lblTime.Height);
+            lblDate.Location = new Point(x2, y2 + 2*lblTime.Height);
         }
 
         private void lblXemLsgd_Click(object sender, EventArgs e)
