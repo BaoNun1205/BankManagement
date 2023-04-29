53a7bada81fce2fdd14489d3698108619d57203d
BaoNun1205 <baole.120503@gmail.com>
2023-04-29 21:45:58 +0700

    oke tiết kiệm

--- DOAN_Nhom4/FrmTietkiem.cs
+++ DOAN_Nhom4/FrmTietkiem.cs
@@ -52,6 +52,7 @@ namespace DOAN_Nhom4
 
         private void FrmTietkiem_Load(object sender, EventArgs e)
         {
+            tkiemDAO.KiemTraSoTietKiem(tknh);
             rand = new Random();
             maTietKiem = rand.Next(100000, 999999);
             foreach (LoaiSoTietKiem gt in Enum.GetValues(typeof(LoaiSoTietKiem)))
@@ -71,7 +72,6 @@ namespace DOAN_Nhom4
                 if (loai == LoaiSoTietKiem.Loai1) loaiSo = 1;
                 if (loai == LoaiSoTietKiem.Loai2) loaiSo = 2;
                 if (loai == LoaiSoTietKiem.Loai3) loaiSo = 3;
-                //Sử dụng giá trị enum và nội dung của nó ở đây
             }
         }
 
@@ -90,23 +90,37 @@ namespace DOAN_Nhom4
                 txtSotien.Text = "Bạn muốn gửi bao nhiêu?";
             }    
         }
-        private void btn1thang_Click(object sender, EventArgs e)
+
+        private void HienThiThongTin()
         {
-            kiHan = 1;
-            lblLaisuat.Text = "6%";
-            laiSuat = 0.06;
-            if (txtSotien.Text != "Bạn muốn gửi bao nhiêu?")
+            BigInteger kt;
+            if (BigInteger.TryParse(txtSotien.Text, out kt))
             {
-                if (tkiemDAO.ktSotien(int.Parse(txtSotien.Text), tknh.SoDu) == true)
+                if (tkiemDAO.ktSotien(BigInteger.Parse(txtSotien.Text), tknh.SoDu) == true)
                 {
-                    tienGoc = int.Parse(txtSotien.Text);
+                    tienGoc = BigInteger.Parse(txtSotien.Text);
                     tienLai = tkiemDAO.TienLai(tienGoc, laiSuat, kiHan);
                     lblTongtienlai.Text = tienLai.ToString();
                     tongTien = tkiemDAO.TongTien(tienGoc, tienLai);
                     lblTongtien.Text = tongTien.ToString();
                 }
                 else { MessageBox.Show("Vui lòng nhập lại số tiền.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); }
-            }
+            }          
+        }
+
+        private bool KiemTraThongTin()
+        {
+            if (txtTenTKTK.Text == "" || cbLoaiSTK.Text == "")
+            { return false; }
+            else { return true; }
+        }
+        private void btn1thang_Click(object sender, EventArgs e)
+        {
+            kiHan = 1;
+            lblLaisuat.Text = "6%";
+            laiSuat = 0.06;
+            HienThiThongTin();
+
               btn1thang.ForeColor = Color.White;
               btn1thang.BackColor = Color.FromArgb(2, 69, 158);
               btn3thang.ForeColor = Color.FromArgb(2, 69, 158);
@@ -124,18 +138,8 @@ namespace DOAN_Nhom4
             kiHan = 3;
             lblLaisuat.Text = "6%";
             laiSuat = 0.06;
-            if (txtSotien.Text != "Bạn muốn gửi bao nhiêu?")
-            {
-                if (tkiemDAO.ktSotien(int.Parse(txtSotien.Text), tknh.SoDu) == true)
-                {
-                    tienGoc = int.Parse(txtSotien.Text);
-                    tienLai = tkiemDAO.TienLai(tienGoc, laiSuat, kiHan);
-                    lblTongtienlai.Text = tienLai.ToString();
-                    tongTien = tkiemDAO.TongTien(tienGoc, tienLai);
-                    lblTongtien.Text = tongTien.ToString();
-                }
-                else { MessageBox.Show("Vui lòng nhập lại số tiền.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); }
-            }
+            HienThiThongTin();
+
             btn3thang.ForeColor = Color.White;
             btn3thang.BackColor = Color.FromArgb(2, 69, 158);
             btn1thang.ForeColor = Color.FromArgb(2, 69, 158);
@@ -153,18 +157,8 @@ namespace DOAN_Nhom4
             kiHan = 6;
             lblLaisuat.Text = "8%";
             laiSuat = 0.08;
-            if (txtSotien.Text != "Bạn muốn gửi bao nhiêu?")
-            {
-                if (tkiemDAO.ktSotien(int.Parse(txtSotien.Text), tknh.SoDu) == true)
-                {
-                    tienGoc = int.Parse(txtSotien.Text);
-                    tienLai = tkiemDAO.TienLai(tienGoc, laiSuat, kiHan);
-                    lblTongtienlai.Text = tienLai.ToString();
-                    tongTien = tkiemDAO.TongTien(tienGoc, tienLai);
-                    lblTongtien.Text = tongTien.ToString();
-                }
-                else { MessageBox.Show("Vui lòng nhập lại số tiền.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); }
-            }
+            HienThiThongTin();
+
             btn6thang.ForeColor = Color.White;
             btn6thang.BackColor = Color.FromArgb(2, 69, 158);
             btn3thang.ForeColor = Color.FromArgb(2, 69, 158);
@@ -182,18 +176,8 @@ namespace DOAN_Nhom4
             kiHan = 12;
             lblLaisuat.Text = "8,6%";
             laiSuat = 0.086;
-            if (txtSotien.Text != "Bạn muốn gửi bao nhiêu?")
-            {
-                if (tkiemDAO.ktSotien(int.Parse(txtSotien.Text), tknh.SoDu) == true)
-                {
-                    tienGoc = int.Parse(txtSotien.Text);
-                    tienLai = tkiemDAO.TienLai(tienGoc, laiSuat, kiHan);
-                    lblTongtienlai.Text = tienLai.ToString();
-                    tongTien = tkiemDAO.TongTien(tienGoc, tienLai);
-                    lblTongtien.Text = tongTien.ToString();
-                }
-                else { MessageBox.Show("Vui lòng nhập lại số tiền.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); }
-            }
+            HienThiThongTin();
+
             btn12thang.ForeColor = Color.White;
             btn12thang.BackColor = Color.FromArgb(2, 69, 158);
             btn3thang.ForeColor = Color.FromArgb(2, 69, 158);
@@ -211,18 +195,8 @@ namespace DOAN_Nhom4
             kiHan = 18;
             lblLaisuat.Text = "9%";
             laiSuat = 0.09;
-            if (txtSotien.Text != "Bạn muốn gửi bao nhiêu?")
-            {
-                if (tkiemDAO.ktSotien(int.Parse(txtSotien.Text), tknh.SoDu) == true)
-                {
-                    tienGoc = int.Parse(txtSotien.Text);
-                    tienLai = tkiemDAO.TienLai(tienGoc, laiSuat, kiHan);
-                    lblTongtienlai.Text = tienLai.ToString();
-                    tongTien = tkiemDAO.TongTien(tienGoc, tienLai);
-                    lblTongtien.Text = tongTien.ToString();
-                }
-                else { MessageBox.Show("Vui lòng nhập lại số tiền.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); }
-            }
+            HienThiThongTin();
+
             btn18thang.ForeColor = Color.White;
             btn18thang.BackColor = Color.FromArgb(2, 69, 158);
             btn3thang.ForeColor = Color.FromArgb(2, 69, 158);
@@ -237,16 +211,20 @@ namespace DOAN_Nhom4
 
         private void btnXacnhan_Click(object sender, EventArgs e)
         {
-            tknh.SoDu = tknh.SoDu - tienGoc;
-            tknhDAO.Sua(tknh);
-            tenTKTK = txtTenTKTK.Text;
-            TietKiem tkiem = new TietKiem(maTietKiem, DateTime.Now, tenTKTK, loaiSo, tienGoc, kiHan, laiSuat, tienLai, tongTien);
-            GiaoDich gd = new GiaoDich("Gui tiet kiem", kh.TenNH, kh.TenTK, kh.SoTK, "HHB", txtTenTKTK.Text, maTietKiem.ToString(), int.Parse(txtSotien.Text), "");
-            tkiemDAO.Them(tkiem);
-            lsgdDAO.Them(gd);
-            MessageBox.Show("Tạo tài khoản tiết kiệm thành công.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            FrmTietkiem frmtietkiem = new FrmTietkiem(kh, tknh, pnlNguoidung);
-            DOAN_Nhom4.ClassAddForm.addForm(frmtietkiem, pnlNguoidung);
+            if (KiemTraThongTin() == true)
+            {
+                tknh.SoDu = tknh.SoDu - tienGoc;
+                tknhDAO.Sua(tknh);
+                tenTKTK = txtTenTKTK.Text;
+                TietKiem tkiem = new TietKiem(maTietKiem, DateTime.Now, tenTKTK, loaiSo, tienGoc, kiHan, laiSuat, tienLai, tongTien);
+                GiaoDich gd = new GiaoDich("Gui tiet kiem", kh.TenNH, kh.TenTK, kh.SoTK, "HHB", txtTenTKTK.Text, maTietKiem.ToString(), int.Parse(txtSotien.Text), "");
+                tkiemDAO.Them(tkiem);
+                lsgdDAO.Them(gd);
+                MessageBox.Show("Tạo tài khoản tiết kiệm thành công.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                FrmTietkiem frmtietkiem = new FrmTietkiem(kh, tknh, pnlNguoidung);
+                DOAN_Nhom4.ClassAddForm.addForm(frmtietkiem, pnlNguoidung);
+            }    
+            else MessageBox.Show("Vui lòng điền đầy đủ các thông tin.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void lblTKTK_Click(object sender, EventArgs e)
@@ -254,6 +232,8 @@ namespace DOAN_Nhom4
             FrmXemTKTK frmxemtktk = new FrmXemTKTK(kh, tknh, pnlNguoidung);
             DOAN_Nhom4.ClassAddForm.addForm(frmxemtktk, pnlNguoidung);
         }
+
+
 
         private void pnlTietkiem_Paint(object sender, PaintEventArgs e)
         {
--- DOAN_Nhom4/TietkiemDAO.cs
+++ DOAN_Nhom4/TietkiemDAO.cs
@@ -13,6 +13,7 @@ namespace DOAN_Nhom4
     internal class TietkiemDAO
     {
         DBConnection data = new DBConnection();
+        TaiKhoanNganHangDAO tknhDAO = new TaiKhoanNganHangDAO();
         public TietkiemDAO() { }
 
         public TietKiem LayThongTinSoTietKiem(string Cot, string giaTri)
@@ -57,11 +58,59 @@ namespace DOAN_Nhom4
             data.xuLi(sqlTkiem);
         }
 
+        public void Sua(TietKiem tkiem)
+        {
+            string sqlTkiem = string.Format("UPDATE TaiKhoanTietKiem SET NgayDangKy = '{0}', TienGoc = '{1}', TienLai = '{2}', TongTien = {3} WHERE MaTietKiem = '{4}'", tkiem.NgayDangky, tkiem.TienGoc, tkiem.TienLai, tkiem.TongTien, tkiem.MaTietKiem);
+            data.xuLi(sqlTkiem);
+        }
+
+
         public DateTime NgayToiHan(DateTime ngaydangky, int kihan)
         {
             TimeSpan timeToAdd = TimeSpan.FromDays(kihan*30);
             DateTime ngaytoihan = ngaydangky.Add(timeToAdd);
             return ngaytoihan;
+        }
+
+        public void KiemTraSoTietKiem(TaiKhoanNganHang tknh)
+        {
+            List<string> matks = new List<string>();
+            List<TietKiem> tkiems = new List<TietKiem>();
+            matks = LayCotTKTK("MaTietKiem");
+            foreach (string matk in matks)
+            {
+                TietKiem tkiem = new TietKiem();
+                tkiem = LayThongTinSoTietKiem("MaTietKiem", matk);
+                tkiems.Add(tkiem);
+            }
+            
+            foreach (TietKiem tk in tkiems)
+            {
+                if (NgayToiHan(tk.NgayDangky, tk.KiHan).ToString("dd/MM/yyyy") == DateTime.Now.ToString("dd/MM/yyyy"))
+                {
+                    if (tk.LoaiSo == 1)
+                    {
+                        tknh.SoDu = tknh.SoDu + tk.TongTien;
+                        Rut(tk);
+                        tknhDAO.SuaSoDu(tknh);
+                    }
+                    if (tk.LoaiSo == 2)
+                    {
+                        tknh.SoDu = tknh.SoDu + tk.TienLai;
+                        tk.NgayDangky = DateTime.Now;
+                        Sua(tk);
+                        tknhDAO.SuaSoDu(tknh);
+                    }
+                    if (tk.LoaiSo == 3)
+                    {
+                        tk.TienGoc = tk.TongTien;
+                        tk.NgayDangky = DateTime.Now;
+                        tk.TienLai = TienLai(tk.TienGoc, tk.LaiSuat, tk.KiHan);
+                        tk.TongTien = TongTien(tk.TienGoc, tk.TienLai);
+                        Sua(tk);
+                    }                        
+                }
+            }    
         }
     }
 }
