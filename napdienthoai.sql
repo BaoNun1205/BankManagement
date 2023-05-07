dc64d98aa70a20df156e87a4e197016ddfcb901f
hieulag15 <hieuthanhtran12@gmail.com>
2023-05-08 00:45:31 +0700

    nap tien dien thoai

---
+++ napdienthoai.sql
@@ -0,0 +1,9 @@
+﻿create table SoDienThoai(
+	SDT VARCHAR(11),
+	SoDu BigInt
+)
+
+insert into SoDienThoai values('0383738129', 1000000)
+insert into SoDienThoai values('0383738128', 1000000)
+insert into SoDienThoai values('0383738127', 1000000)
+insert into SoDienThoai values('0383738126', 1000000)
